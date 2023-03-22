6d6c7a9790ee8a5e3ab035fca40fab131bc50e2c
Dungyy <erickmunoz13@gmail.com>
2023-03-21 20:33:32 -0500

    So much to learn lol

---
+++ IntroLibrary/PersonModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace IntroLibrary 
+{
+    public class PersonalModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
--- IntroUI/Program.cs
+++ IntroUI/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using IntroLibrary;
+
+
+namespace IntroUI 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ");
+            PersonalModel p = new PersonalModel
+            {
+                FirstName = "Erick",
+                LastName = "Munoz"
+            };
+
+            System.Console.WriteLine($"My Name is { p.FirstName } { p.LastName } ");
+        }
+    }
+}
